4a52d8f130bbad246c2442cf9d5e3016e34997a1
jessicaoviedo <113190722+jessicaoviedo@users.noreply.github.com>
2022-11-03 10:16:45 -0400

    uciqcguiu

---
+++ index.js
@@ -0,0 +1,14 @@
+/* const DOMSelectors = {
+  button: document.getElementById("btn"),
+  text: document.querySelector("#text"),
+  box: document.getElementById("big-black-box"),
+  points: document.querySelectorAll(".point"),
+};
+
+function backgroundAndText(background, text) {
+  background.style.backgroundColor = "red";
+  text.textContent = "this is now a bigger red box";
+  text.style.fontSize = "40px";
+}
+
+backgroundAndText(DOMSelectors.box, DOMSelectors.text); */
