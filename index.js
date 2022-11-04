6ad81a74b85a70f52d823154857369054fd551c1
jessicaoviedo <113190722+jessicaoviedo@users.noreply.github.com>
2022-11-04 10:14:22 -0400

    wgfiweu

--- index.js
+++ index.js
@@ -1,3 +1,49 @@
+const DOMSelectors = {
+  button: document.getElementById("btn"),
+  text: document.querySelector("#text"),
+  box: document.getElementById("big-grey-box"),
+  points: document.querySelectorAll(".point"), //node list
+};
+
+function backgroundAndText(background, text) {
+  background.style.backgroundColor = "grey";
+  text.textContent = " and now this is a list of avengers";
+  text.style.fontSize = "40px";
+}
+
+DOMSelectors.button.addEventListener("click", function () {
+  backgroundAndText(DOMSelectors.box, DOMSelectors.text);
+});
+
+function changeLi() {
+  DOMSelectors.points.forEach((point) => {
+    point.addEeventListener;
+    "click",
+      function () {
+        point.textContent("hello");
+      };
+  });
+}
+
+const qsa = (els) => document.querySelectorAll(els);
+
+const allAvengers = qsa(".avenger");
+console.log(allAvengers);
+
+const allAvengersArray = Array.from(allAvengers);
+
+const originalAvengers = allAvengersArray.filter((node) => {
+  if (node.classList[1] === "original") {
+    return true;
+  }
+  return false;
+});
+console.log(originalAvengers);
+
+const associatesArray = Array.from(qsa(".associate"));
+const associates = associatesArray.map((item) => item.innerHTML);
+console.log(associates);
+
 /* const DOMSelectors = {
   button: document.getElementById("btn"),
   text: document.querySelector("#text"),
