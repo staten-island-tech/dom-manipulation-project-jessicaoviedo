09351d421d97291ee5e18c250861cbb0809723ab
jessicaoviedo <113190722+jessicaoviedo@users.noreply.github.com>
2022-11-17 10:13:59 -0500

    xcqwfqfw

--- index.js
+++ index.js
@@ -1,63 +1,14 @@
 const DOMSelectors = {
-  submit: document.querySelector("#form"),
-  text: document.querySelector("#text"),
-  box: document.getElementById("big-black-box"),
-  points: document.querySelectorAll(".point"),
+  form: document.querySelector("#form"),
+  title: document.querySelector("title"),
+  artist: document.getElementById("artist"),
+  url: document.querySelectorAll("url"),
 };
 
-DOMSelectors.submit.addEventListener("click", function (e) {
-  let input = DOMSelectors.submit.value;
+DOMSelectors.form.addEventListener("submit", function (e) {
   e.preventDefault();
-  DOMSelectors.box.insertAdjacentHTML("beforeend", `<p>${input}</p>`);
   console.log(e);
+  let title = DOMSelectors.title.submit;
+  let artist = DOMSelectors.artist.submit;
+  let url = DOMSelectors.url.submit;
 });
-
-/* const title = "";
-console.log(DOMSelectors.title);
-const StarWars = {
-  title: "star wars",
-};
-
-function makeCard() {
-  //make the card
-  //get delete button
-  //querySelectorAll returns node list
-  //for each btuton add event listener
-  //function(e) event.target
-}
-
-document.getElementById("form").addEventListener("submit", function (e) {
-  e.preventDefault();
-}); */
-
-//const DOMSelectors = {
-//title: document.getElementById("title"),
-//artist: document.getElementById("artist"),
-//url: document.getElementById("url"),
-//display: document.getElementById("display"),
-//};
-
-/* DOMSelectors.button.addEventListener("click", function () {
-  let input = DOMSelectors.input.value;
-  DOMSelectors.box.insertAdjacentHTML("beforeend", `<p>${input}</p>`);
-  DOMSelectors.input.value = "";
-}); */
-
-// step 1- get data from user, take the data save to it a variable
-// step 2- create an object from that
-// step 3 - push that data into html
-
-/* const DOMSelectors = {
-  button: document.getElementById("btn"),
-  text: document.querySelector("#text"),
-  box: document.getElementById("big-black-box"),
-  points: document.querySelectorAll(".point"),
-};
-
-function backgroundAndText(background, text) {
-  background.style.backgroundColor = "red";
-  text.textContent = "this is now a bigger red box";
-  text.style.fontSize = "40px";
-}
-
-backgroundAndText(DOMSelectors.box, DOMSelectors.text); */
