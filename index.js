350926e2358b847268b8c34f87d5f608e762e21c
jessicaoviedo <113190722+jessicaoviedo@users.noreply.github.com>
2022-11-18 10:15:27 -0500

    adevswfrbe

--- index.js
+++ index.js
@@ -1,14 +1,19 @@
 const DOMSelectors = {
   form: document.querySelector("#form"),
-  title: document.querySelector("title"),
+  albumname: document.getElementById("albumname"),
   artist: document.getElementById("artist"),
-  url: document.querySelectorAll("url"),
+  url: document.getElementById("url"),
 };
 
 DOMSelectors.form.addEventListener("submit", function (e) {
   e.preventDefault();
   console.log(e);
-  let title = DOMSelectors.title.value;
+  let albumname = DOMSelectors.albumname.value;
   let artist = DOMSelectors.artist.value;
   let url = DOMSelectors.url.value;
+  console.log(albumname);
+  console.log(artist);
+  console.log(url);
 });
+
+DOMSelectors.form.insertAdjacentHTML("beforeend", `cards`);
